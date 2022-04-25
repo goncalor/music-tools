506702c024f6e4736ae04344f7c8c32ade57805e
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2022-04-25 18:54:53 +0100

    Write script to save lyrics for mp3 files

--- download-lyrics.py
+++ download-lyrics.py
@@ -1,58 +1,18 @@
 #!/usr/bin/env python3
+import os
 import sys
-import string
-import lxml.html
-import requests
-import xml.etree.ElementTree as ET
+import eyed3
+import lyrics as lyr
 
-BASE_URL = "https://www.azlyrics.com/lyrics/"
-USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0"
+for mp3 in sys.argv[1:]:
+    tag = eyed3.load(mp3).tag
+    artist = tag.artist
+    title = tag.title
 
+    lyrics = lyr.download_lyrics(artist, title)
 
-def extract_lyrics(page):
-    html = lxml.html.fromstring(page)
+    (root, ext) = os.path.splitext(mp3)
+    out_path = root + ".txt"
 
-    for div in html.findall(".//*div"):
-        if not div.attrib:
-            lyrics = ET.tostringlist(div, encoding="unicode", method="text")
-            lyrics = [s.strip() for s in lyrics]
-
-            tmp = list()
-            start = 0
-            end = len(lyrics)
-            for l in lyrics:
-                if l == "":
-                    start += 1
-                else:
-                    break
-            for l in lyrics[::-1]:
-                if l == "":
-                    end -= 1
-                else:
-                    break
-            return "\n".join(lyrics[start:end])
-
-
-def normalise(name):
-    name = name.lower()
-    name = "".join([a for a in name if a in string.ascii_letters])
-
-    return name
-
-
-def download_lyrics(artist, title):
-    artist = normalise(artist)
-    title = normalise(title)
-
-    url = "{}{}/{}.html".format(BASE_URL, artist, title)
-
-    r = requests.get(url, headers={'user-agent': USER_AGENT})
-    r.raise_for_status()
-
-    lyrics = extract_lyrics(r.text)
-
-    return lyrics
-
-
-if __name__ == "__main__":
-    print(download_lyrics(sys.argv[1], sys.argv[2]))
+    with open(out_path, "w") as f:
+        f.write(lyrics)
