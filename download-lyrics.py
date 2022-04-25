eeed69bb2f753292470240f19b8463b8a3543a02
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2022-04-25 18:10:14 +0100

    Write download-lyrics.py

---
+++ download-lyrics.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import sys
+import string
+import lxml.html
+import requests
+import xml.etree.ElementTree as ET
+
+BASE_URL = "https://www.azlyrics.com/lyrics/"
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:98.0) Gecko/20100101 Firefox/98.0"
+
+
+def extract_lyrics(page):
+    html = lxml.html.fromstring(page)
+
+    for div in html.findall(".//*div"):
+        if not div.attrib:
+            lyrics = ET.tostringlist(div, encoding="unicode", method="text")
+            lyrics = [s.strip() for s in lyrics]
+
+            tmp = list()
+            start = 0
+            end = len(lyrics)
+            for l in lyrics:
+                if l == "":
+                    start += 1
+                else:
+                    break
+            for l in lyrics[::-1]:
+                if l == "":
+                    end -= 1
+                else:
+                    break
+            return "\n".join(lyrics[start:end])
+
+
+def normalise(name):
+    name = name.lower()
+    name = "".join([a for a in name if a in string.ascii_letters])
+
+    return name
+
+
+artist = normalise(sys.argv[1])
+title = normalise(sys.argv[2])
+
+url = "{}{}/{}.html".format(BASE_URL, artist, title)
+
+r = requests.get(url, headers={'user-agent': USER_AGENT})
+r.raise_for_status()
+
+lyrics = extract_lyrics(r.text)
+print(lyrics)
