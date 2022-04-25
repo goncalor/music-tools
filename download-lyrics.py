96ddd7bcf48c9205a3f57cbd324244e5390526ce
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2022-04-25 19:23:30 +0100

    Handle downloading exceptions. Log messages

--- download-lyrics.py
+++ download-lyrics.py
@@ -2,17 +2,29 @@
 import os
 import sys
 import eyed3
+import logging
 import lyrics as lyr
 
+logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S %z',
+                    level=logging.INFO)
+
 for mp3 in sys.argv[1:]:
+    logging.debug("Loading {}".format(mp3))
     tag = eyed3.load(mp3).tag
     artist = tag.artist
     title = tag.title
+    logging.info("Getting lyrics for '{}' by '{}'".format(title, artist))
 
-    lyrics = lyr.download_lyrics(artist, title)
+    try:
+        lyrics = lyr.download_lyrics(artist, title)
+    except Exception as e:
+        logging.error("Failed with error {}".format(e))
+        continue
 
     (root, ext) = os.path.splitext(mp3)
     out_path = root + ".txt"
 
+    logging.debug("Writing to {}".format(out_path))
     with open(out_path, "w") as f:
         f.write(lyrics)
